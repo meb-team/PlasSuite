1ae4fbbad1f3eec27c7ebefd25766a2e9a045dac
chilpert2 <cecile.hilpert@gmail.com>
2018-11-28 12:05:21 +0100

    Add R scripts for simulation graphs and taxo

---
+++ bin/drawTaxo.R
@@ -0,0 +1,28 @@
+library(ggplot2) 
+
+biofilm=read.table("/databis/hilpert/resultsPlasTaxo/NG-14342_NG-17411_BIOFILM.megahit.taxo.predicted_plasmids",sep="\t") 
+step2=read.table("/databis/hilpert/resultsPlasTaxo/NG-18198_STEP10.megahit.taxo.predicted_plasmids",sep="\t") 
+feces1=read.table("/databis/hilpert/resultsPlasTaxo/NG-18198_Fe1210.megahit.taxo.predicted_plasmids",sep="\t") 
+pavin=read.table("/databis/hilpert/resultsPlasTaxo/NG-18198_Pavin10.megahit.taxo.predicted_plasmids",sep="\t") 
+
+biofilm$Sample="Biofilm" 
+step2$Sample="STEP2" 
+feces1$Sample="Feces1" 
+pavin$Sample="Pavin" 
+
+taxo_plasmids=rbind(biofilm,step2,feces1,pavin) 
+colors=c("#EB466E", "#62c36e", "#ffe746", "#6882df", "#f79b5a", "#a74ac3", "#67dcf6", "#f35aeb")
+
+taxoPerc=ggplot(taxo_plasmids,aes(x=Sample,fill=V2))+geom_bar(position="fill")+scale_fill_manual(values=colors)+scale_y_continuous(labels=scales::percent)+labs(fill="Taxon",y="% taxon")+theme(legend.text=element_text(size=14),legend.title=element_text(size=16),axis.text.x=element_text(size=14),axis.title.x=element_text(size=16),axis.text.y=element_text(size=14),axis.title.y=element_text(size=16))
+
+taxoCount=ggplot(taxo_plasmids,aes(x=Sample,fill=V2))+geom_bar()+scale_fill_manual(values=colors)+labs(fill="Taxon",y="Number of contigs")+theme(legend.text=element_text(size=14),legend.title=element_text(size=16),axis.text.x=element_text(size=14),axis.title.x=element_text(size=16),axis.text.y=element_text(size=14),axis.title.y=element_text(size=16))
+
+pdf("/databis/hilpert/resultsPlasTaxo/taxo_percent.pdf")
+taxoPerc
+dev.off() 
+
+pdf("/databis/hilpert/resultsPlasTaxo/taxo_count.pdf") 
+taxoCount 
+dev.off() 
+
+save.image(file="/databis/hilpert/R_images/taxo.Rdata") 
