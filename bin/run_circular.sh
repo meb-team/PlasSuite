8dd5b1fd4c1162660c486e70a6f012063d5508d7
chilpert2 <cecile.hilpert@gmail.com>
2020-02-13 11:33:00 +0100

    move circular detection script

--- bin/run_circular.sh
+++ bin/run_circular.sh
@@ -9,9 +9,18 @@ if [[ "$#" -ne 2 ]]; then
 	exit 1 
 fi 
 
-tmp=`mktemp -d -p .`
+tool_dir=$(echo $0 | rev | cut -f 3- -d "/" | rev)
+if [[ $tool_dir == "" ]]; then
+        tool_dir="."
+elif [[ $tool_dir == $0 ]]; then
+        tool_dir=".."
+fi
+BIN=$tool_dir/bin
 
+
+tmp=`mktemp -d -p .`
+echo $tmp
 awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);} END {printf("\n");}' $1 > $tmp/oneline.fasta
-/data/chochart/lib/circular_detection/bin/circular_detection.pl -f $tmp/oneline.fasta -k 100 --only_circular > $2.fasta
+$BIN/circular_detection.pl -f $tmp/oneline.fasta -k 100 --only_circular > $2.fasta
 grep "^>" $2.fasta | cut -f 1 -d " " | tr -d ">" > $2.id  
-rm -r $tmp 
+#rm -r $tmp 
